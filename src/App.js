6b9b42070b9c196a3e18e747f406990bd127304c
robinrabauke <robin.mesaric@adac-camping.de>
2020-08-26 16:32:39 +0200

    Add graphcms

--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
-import React from 'react';
-import stock1 from './stocks/Stock1.jpg';
-import stock2 from './stocks/Stock2.jpg';
-import stock3 from './stocks/Stock3.jpg';
-import stock4 from './stocks/Stock4.jpg';
-import stock5 from './stocks/Stock5.jpg';
-import stock6 from './stocks/Stock6.jpg';
-import stock7 from './stocks/Stock7.jpg';
-import stock8 from './stocks/Stock8.jpg';
-import stock9 from './stocks/Stock9.jpg';
-import stock10 from './stocks/Stock10.jpg';
-import stock11 from './stocks/Stock11.jpg';
-import stock12 from './stocks/Stock12.jpg';
-import stock13 from './stocks/Stock13.jpg';
-import stock14 from './stocks/Stock14.jpg';
-import stock15 from './stocks/Stock15.jpg';
-import stock16 from './stocks/Stock16.jpg';
-import stock17 from './stocks/Stock17.jpg';
-import stock18 from './stocks/Stock18.jpg';
-import stock19 from './stocks/Stock19.jpg';
-import stock20 from './stocks/Stock20.jpg';
-import stock21 from './stocks/Stock21.jpg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
+import { request } from 'graphql-request';
+import GraphImg from 'graphcms-image'
 
-function App() {
+export const App = () => {
 
-  const stocks = [stock1, stock2, stock3, stock4, stock5, stock6, stock7, stock8, stock9, stock10, stock11, stock12, stock13, stock14, stock15, stock16, stock17, stock18, stock19, stock20, stock21];
+  const [stocks, setStocks] = useState(null);
 
-  const randomStock = stocks[Math.floor(Math.random() * stocks.length)];
+  useEffect(() => {
+    const fetchStocks = async () => {
+      const { stocks } = await request(
+        'https://api-eu-central-1.graphcms.com/v2/ckebe31k90oeq01xv1p012e1a/master',
+        `
+      { 
+        stocks {
+          description
+          image {
+            handle
+            width
+            height
+            url
+          }
+        }
+      }
+    `
+      );
 
+      setStocks(stocks);
+    };
 
+    fetchStocks();
+  }, []);
 
+  // const stocks = [stock1, stock2, stock3, stock4, stock5, stock6, stock7, stock8, stock9, stock10, stock11, stock12, stock13, stock14, stock15, stock16, stock17, stock18, stock19, stock20, stock21];
+
+  const randomStock = stocks && stocks[Math.floor(Math.random() * stocks.length)];
+
+  console.log(randomStock && randomStock.image);
+  const stockDescriptoion = randomStock && { __html: randomStock.description }
   return (
-    <div>
-      <div style={{
-        backgroundImage: `url(${randomStock})`
-      }} className="stockwrapper">
-
-      </div >
-      <div className="footer">
-        <span className="footer-item">Made with {'<3'} for stock-footage in Berlin</span>
-        <span className="footer-item"><a className="footer-link" href='/impressum'>impressum</a></span>
+    <>
+      <div className="stockwrapper">
+        {/* {randomStock && <GraphImg className="stock" fadeIn image={randomStock.image} />} */}
+        {randomStock
+          && <div className='stock' style={{
+            backgroundImage: `url(${randomStock.image.url})`
+          }} > </div>}
+        <div className="footer">
+          <span className="footer-item">Made with {'<3'} for stock-footage in Berlin</span>
+          <span className="footer-link" dangerouslySetInnerHTML={randomStock && stockDescriptoion}></span>
+          <span className="footer-item"><a className="footer-link" href='/impressum'>impressum</a></span>
+        </div >
       </div>
-    </div>
+
+    </>
   );
 }
 
