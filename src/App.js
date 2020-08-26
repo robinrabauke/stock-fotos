ad9e2bf9abdc190aa892959c02f26b0b3f480531
robinrabauke <robin.mesaric@adac-camping.de>
2020-08-26 10:55:58 +0200

    Add Stocks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import stock1 from './stocks/Stock1.jpg';
+import stock2 from './stocks/Stock2.jpg';
+import stock3 from './stocks/Stock3.jpg';
+import stock4 from './stocks/Stock4.jpg';
+import stock5 from './stocks/Stock5.jpg';
+import stock6 from './stocks/Stock6.jpg';
+import stock7 from './stocks/Stock7.jpg';
 import './App.css';
+import "animate.css/animate.min.css";
+
+import ScrollAnimation from 'react-animate-on-scroll'
 
 function App() {
+
+  const stocks = [stock1, stock2, stock3, stock4, stock5, stock6, stock7];
+
+  const randomStock = stocks[Math.floor(Math.random() * stocks.length)];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="stockwrapper">
+        <img src={randomStock} className="stock" alt="stock" />
+      </div >
     </div>
   );
 }
